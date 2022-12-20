fe59a1bd5cdabbe6a175d01af6dfbc44a4d63d2f
Martin Renner <martin+github@planet-renner.net>
2022-12-20 20:30:05 +0100

    Error handling if the "build" script cannot be deleted.
    Because Stream Deck has a lock on it if the plugin is installed directly from this folder by double click.

--- release.bat
+++ release.bat
@@ -4,8 +4,15 @@ setlocal
 dotnet publish StreamDeckSimHub.Plugin\StreamDeckSimHub.Plugin.csproj -c Release -r win-x64 --self-contained
 
 if exist build rmdir /s /q build
+if exist build goto :endFailure
 mkdir build
 xcopy StreamDeckSimHub.Plugin\bin\Release\net6.0\win-x64\publish build\net.planetrenner.simhub.sdPlugin /e /i
 
 cd build
 ..\..\DistributionTool.exe -b -i net.planetrenner.simhub.sdPlugin -o .
+goto end
+
+:endFailure
+exit /b 1
+
+:end
