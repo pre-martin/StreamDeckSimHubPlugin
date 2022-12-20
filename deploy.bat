9b382088c5556fb5d84d53547b7843b40d48f977
Martin Renner <martin+github@planet-renner.net>
2022-12-20 20:29:08 +0100

    Script to deploy locally.

---
+++ deploy.bat
@@ -0,0 +1,13 @@
+﻿@echo off
+setlocal
+
+rem Script to deploy locally.
+rem Also allows to deploy with the same "version" in manifest.json as already installed.
+
+taskkill /im StreamDeck.exe /t /f
+
+cd build
+xcopy net.planetrenner.simhub.sdPlugin "%AppData%\Elgato\StreamDeck\Plugins\net.planetrenner.simhub.sdPlugin\" /e /y
+cd ..
+
+start /d "%ProgramFiles%\Elgato\StreamDeck" StreamDeck.exe
