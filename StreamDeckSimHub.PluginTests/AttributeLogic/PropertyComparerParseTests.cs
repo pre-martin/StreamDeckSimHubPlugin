0b45d5a71bf87a0ed9a15a45b9e6700c4463bbd4
Martin Renner <martin+github@planet-renner.net>
2023-01-02 23:03:00 +0100

    Some tests for the expression parser.
    Issue #10

---
+++ StreamDeckSimHub.PluginTests/AttributeLogic/PropertyComparerParseTests.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (C) 2022 Martin Renner
+// LGPL-3.0-or-later (see file COPYING and COPYING.LESSER)
+
+using Microsoft.Extensions.Logging;
+using StreamDeckSimHub.Plugin.PropertyLogic;
+
+namespace StreamDeckSimHub.PluginTests.AttributeLogic;
+
+public class PropertyComparerParseTests
+{
+    private PropertyComparer _propertyComparer;
+
+    [SetUp]
+    public void Init()
+    {
+        var logger = Mock.Of<ILogger<PropertyComparer>>();
+        _propertyComparer = new PropertyComparer(logger);
+    }
+
+    [Test]
+    public void TestParseEquals()
+    {
+        var ce = _propertyComparer.Parse("property.name==5");
+        Assert.That(ce.Property, Is.EqualTo("property.name"));
+        Assert.That(ce.Operator, Is.EqualTo(ConditionOperator.Eq));
+        Assert.That(ce.CompareValue, Is.EqualTo("5"));
+        
+        var ce2 = _propertyComparer.Parse("  property.name ==  5 ");
+        Assert.That(ce2.Property, Is.EqualTo("property.name"));
+        Assert.That(ce2.Operator, Is.EqualTo(ConditionOperator.Eq));
+        Assert.That(ce2.CompareValue, Is.EqualTo("5"));
+    }
+    
+    [Test]
+    public void TestParseGreaterEquals()
+    {
+        var ce = _propertyComparer.Parse("property.name>=5");
+        Assert.That(ce.Property, Is.EqualTo("property.name"));
+        Assert.That(ce.Operator, Is.EqualTo(ConditionOperator.Ge));
+        Assert.That(ce.CompareValue, Is.EqualTo("5"));
+        
+        var ce2 = _propertyComparer.Parse("  property.name >=  5 ");
+        Assert.That(ce2.Property, Is.EqualTo("property.name"));
+        Assert.That(ce2.Operator, Is.EqualTo(ConditionOperator.Ge));
+        Assert.That(ce2.CompareValue, Is.EqualTo("5"));
+    }
+
+    [Test]
+    public void TestParseNotEquals()
+    {
+        var ce = _propertyComparer.Parse("property.name!=1");
+        Assert.That(ce.Property, Is.EqualTo("property.name"));
+        Assert.That(ce.Operator, Is.EqualTo(ConditionOperator.Ne));
+        Assert.That(ce.CompareValue, Is.EqualTo("1"));
+        
+        var ce2 = _propertyComparer.Parse("  property.name !=  0 ");
+        Assert.That(ce2.Property, Is.EqualTo("property.name"));
+        Assert.That(ce2.Operator, Is.EqualTo(ConditionOperator.Ne));
+        Assert.That(ce2.CompareValue, Is.EqualTo("0"));
+    }
+}
