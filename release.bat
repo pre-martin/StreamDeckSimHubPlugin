642f815ed200b20b989a77691d1fc4a4357e7b49
Martin Renner <martin+github@planet-renner.net>
2023-01-05 17:12:14 +0100

    Converted doc from markdown to asciidoctor.

--- release.bat
+++ release.bat
@@ -1,12 +1,19 @@
 @echo off
 setlocal
 
-dotnet publish StreamDeckSimHub.Plugin\StreamDeckSimHub.Plugin.csproj -c Release -r win-x64 --self-contained
+set CONFIG=Release
+if "%1%" == "debug" set CONFIG=Debug
+
+echo.
+echo Building for configuration: %CONFIG%
+echo.
+
+dotnet publish StreamDeckSimHub.Plugin\StreamDeckSimHub.Plugin.csproj -c %CONFIG% -r win-x64 --self-contained
 
 if exist build rmdir /s /q build
 if exist build goto :endFailure
 mkdir build
-xcopy StreamDeckSimHub.Plugin\bin\Release\net6.0\win-x64\publish build\net.planetrenner.simhub.sdPlugin /e /i
+xcopy StreamDeckSimHub.Plugin\bin\%CONFIG%\net6.0\win-x64\publish build\net.planetrenner.simhub.sdPlugin /e /i
 
 cd build
 ..\..\DistributionTool.exe -b -i net.planetrenner.simhub.sdPlugin -o .
