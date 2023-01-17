16c8857a9224f5f31a2b654497d1076c236521fe
Martin Renner <martin+github@planet-renner.net>
2023-01-17 18:41:03 +0100

    Fix for parsing blank (whitespace) property values
    Issue #22

--- StreamDeckSimHub.Plugin/SimHub/PropertyParser.cs
+++ StreamDeckSimHub.Plugin/SimHub/PropertyParser.cs
@@ -9,9 +9,12 @@ namespace StreamDeckSimHub.Plugin.SimHub;
 public class PropertyParser
 {
 
+    /// <summary>
+    /// Parses a line in the format <c>Property name type value</c>.
+    /// </summary>
     public (string name, PropertyType type, IComparable? value)? ParseLine(string line)
     {
-        var lineItems = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        var lineItems = line.Split(new[] { ' ' }, 4);
         if (lineItems.Length != 4)
         {
             return null;
@@ -22,13 +25,14 @@ public class PropertyParser
         var valueAsString = lineItems[3];
         if (valueAsString == "(null)") valueAsString = null;
 
-        // See https://github.com/pre-martin/SimHubPropertyServer/blob/main/Property/SimHubProperty.cs
+        // See https://github.com/pre-martin/SimHubPropertyServer/blob/main/PropertyServer.Plugin/Property/SimHubProperty.cs
         var type = typeAsString switch
         {
             "boolean" => PropertyType.Boolean,
             "integer" => PropertyType.Integer,
             "long" => PropertyType.Long,
             "double" => PropertyType.Double,
+            "object" => PropertyType.Double,
             _ => PropertyType.Double // Should not happen. But best guess should always be "double".
         };
         var value = type.ParseFromSimHub(valueAsString);
--- StreamDeckSimHub.PluginTests/SimHub/PropertyParserTests.cs
+++ StreamDeckSimHub.PluginTests/SimHub/PropertyParserTests.cs
@@ -8,7 +8,7 @@ namespace StreamDeckSimHub.PluginTests.SimHub;
 public class PropertyParserTests
 {
     private PropertyParser _parser;
-    
+
     [SetUp]
     public void Init()
     {
@@ -23,11 +23,28 @@ public class PropertyParserTests
         Assert.That(result1.Value.name, Is.EqualTo("dcp.gd.SpeedKmh"));
         Assert.That(result1.Value.type, Is.EqualTo(PropertyType.Double));
         Assert.That(result1.Value.value, Is.Null);
-        
+
         var result2 = _parser.ParseLine("Property dcp.gd.SpeedKmh double 18.3334");
         Assert.That(result2, Is.Not.Null);
         Assert.That(result2.Value.name, Is.EqualTo("dcp.gd.SpeedKmh"));
         Assert.That(result2.Value.type, Is.EqualTo(PropertyType.Double));
         Assert.That(result2.Value.value, Is.InRange(18.33, 18.34));
+    }
+
+    [Test]
+    public void ParseWhitespace()
+    {
+        var result1 = _parser.ParseLine("Property DataCorePlugin.ExternalScript.BlinkingGear object  ");
+        Assert.That(result1, Is.Not.Null);
+        Assert.That(result1.Value.name, Is.EqualTo("DataCorePlugin.ExternalScript.BlinkingGear"));
+        Assert.That(result1.Value.type, Is.EqualTo(PropertyType.Double));
+        Assert.That(result1.Value.value, Is.EqualTo(0));
+
+        var result2 = _parser.ParseLine("Property DataCorePlugin.ExternalScript.BlinkingGear object   ");
+        Assert.That(result2, Is.Not.Null);
+        Assert.That(result2.Value.name, Is.EqualTo("DataCorePlugin.ExternalScript.BlinkingGear"));
+        Assert.That(result2.Value.type, Is.EqualTo(PropertyType.Double));
+        Assert.That(result2.Value.value, Is.EqualTo(0));
+
     }
 }
