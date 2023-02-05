42ed8aed372a6806be528ee74e54d00f0d8b1f14
Martin Renner <martin+github@planet-renner.net>
2023-02-05 14:03:38 +0100

    New CPU is much faster, so after killing Stream Deck, a small delay is required.

--- deploy.bat
+++ deploy.bat
@@ -5,6 +5,7 @@ rem Script to deploy locally.
 rem Also allows to deploy with the same "version" in manifest.json as already installed.
 
 taskkill /im StreamDeck.exe /t /f
+timeout 1 > nul
 
 cd build
 xcopy net.planetrenner.simhub.sdPlugin "%AppData%\Elgato\StreamDeck\Plugins\net.planetrenner.simhub.sdPlugin\" /e /y
